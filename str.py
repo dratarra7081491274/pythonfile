e70b083f5336d2607c44401f7ff397e564f019dd
draatarra7081491274 <dratarra2007@gmail.com>
2024-12-12 06:59:57 +0530

    new code by jeany lectur

---
+++ str.py
@@ -0,0 +1,4 @@
+print("string manipulation exercise")
+print('string concaknation is done with "+"sign')
+print('e.g. print("hello"+"Jenny")')
+print("New lines can be created with a backslash and n")
